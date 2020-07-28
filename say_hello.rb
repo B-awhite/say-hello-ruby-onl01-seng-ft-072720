ed3194fc3ddf82dc7ff8dc13e2d1b847b9a97c46
Bria White <bria.awhite@gmail.com>
2020-07-28 19:59:32 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,9 @@
 # Build your say_hello method here
 
-def say_hello
+def say_hello(name = "Ruby Programmer")
   puts "Hello, #{name}!"
 end  
 
-def say_hello
-  puts "Hello Ruby Programmer!"
+def say_hello(name)
+  puts "Hello Kent Beck!"
 end  
